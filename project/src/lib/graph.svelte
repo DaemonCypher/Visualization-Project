37862318d41d637eea9219851c2da6006914ca38
unknown <daemoncypher00@gmail.com>
2025-03-01 18:40:38 -0600

    data

---
+++ project/src/lib/graph.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { Insurance } from "../types";
+    import * as d3 from "d3";
+    // define the props of the Bar component
+    type Props = {
+      movies: TMovie[];
+      progress?: number;
+      width?: number;
+      height?: number;
+    };
+    
+  </script>
---
+++ project/src/types.ts
@@ -0,0 +1,8 @@
+export type Insurance = {
+    age: number;
+    gender:  number;
+    bmi: number;
+    children: number;
+    region: string;
+    charges: number;
+}
