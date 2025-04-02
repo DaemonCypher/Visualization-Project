0c6f1c513ba0ea4478abb77d2f253f89f300c608
Peilin Li <li003997@umn.edu>
2025-04-02 16:04:38 -0500

    Scrolly Story Template created

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,4 +6,5 @@ import Box from "$lib/Box.svelte";
 import Line from "$lib/Line.svelte";
 import BrushScatter from "$lib/BrushScatter.svelte";
 import USMap from "$lib/USMap.svelte";
-export {Frequency, Heat,Scatter,Box,Line,BrushScatter, USMap}
+import Scroll from './Scroll.svelte'
+export {Frequency, Heat,Scatter,Box,Line,BrushScatter, USMap,Scroll}
